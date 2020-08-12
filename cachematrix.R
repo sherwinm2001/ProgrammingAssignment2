c12816b71b5b624cda34805217e4f8f7df409f15
Sherwin Navin Mathias <mathiassherwinn@student.sfit.ac.in>
2020-08-12 13:55:51 +0530

    Solution for creating inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The function defined below creates a special matrix (object) 
+## that gets stored in another environment and has functions to set 
+## and get inverse of the main matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 inv<-NULL
@@ -18,7 +20,10 @@ list(set = set, get = get,
 }
 
 
-## Write a short comment describing this function
+## The following function takes the special matrix created by the above function 
+## as the input and calculates its inverse if it was not calculated before. 
+## If it is already calculated, then it is retrieved from the environment 
+## where it was saved earlier.
 
 cacheSolve <- function(x, ...) {
   inv<-x$getinv()
